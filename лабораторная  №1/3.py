73a28c4c608ab7ce3d51fb83537cd8141cbdc38d
Urusova04 <149236549+Urusova04@users.noreply.github.com>
2023-10-28 16:18:23 +0300

    Add files via upload

---
+++ лабораторная  №1/3.py
@@ -0,0 +1,17 @@
+list_players = ["Маша", "Петя", "Саша", "Оля", "Кирилл", "Коля"]
+list_players1={
+"Маша":0,
+"Петя":1,
+"Саша":2,
+"Оля":3,
+"Кирилл":4,
+"Коля":5
+}
+new_list = len(list_players)
+substring = new_list/2
+players1 = list_players[0:3]
+players2 = list_players[3:6]
+# TODO Разделите участников на две команды
+
+print(players1)
+print(players2)
